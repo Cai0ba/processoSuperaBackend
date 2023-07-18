0f46019e8a562e47348b37e5381806c7b7aa4f5c
Caioba <caio.henrique.1204@gmail.com>
2023-07-17 23:36:06 -0300

    Account implementation

--- src/main/java/br/com/banco/account/Account.java
+++ src/main/java/br/com/banco/account/Account.java
@@ -1,4 +1,23 @@
 package br.com.banco.account;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class Account {
+    @Id
+    @Column(name = "id_conta")
+    private Long id;
+    private String nomeResponsavel;
+
 }
--- src/main/java/br/com/banco/account/AccountRepository.java
+++ src/main/java/br/com/banco/account/AccountRepository.java
@@ -1,4 +1,7 @@
 package br.com.banco.account;
 
-public class AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Account findByNomeResponsavel(String name);
 }
